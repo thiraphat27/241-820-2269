38d667f991b1bf6748e851c7b2005e0bf9afdbc0
thiraphat27 <thiraphat.chat@ku.th>
2025-12-29 17:02:11 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,139 @@
+/*
+ผมไม่ได้มา เนื่องจากกำลังเดินทางจากบ้านไปมอ แล้วเกิดเหตุฉุกเฉินรถพังกระทันหัน ทำให้ผมต้องนำรถกลับมาซ่อมที่บ้านและไม่สามารถมาเรียนได้ในวันนี้ ต้องขออภัยมา ณ ที่นี้ด้วยครับ
+แต่ผมได้เข้ามาเรียนใน Team ตามเวลาที่อาจารย์กำหนดไว้ เพราะกลัวจะเรียนไม่ทันเพื่อนๆครับ 
+กระผม 6721652269 นาย ธีรพัฒน์ จตุวาจา 820 ฝากอาจารย์รับไว้พิจารณาด้วยครับ
+
+//String,Number,Boolean,object,Array
+//1.String
+let name = "John Doe";
+console.log("name:", name);
+
+//2.Number
+let age = 25;
+console.log("age:", age); 
+
+//3.Boolean
+let isStudent = true;
+console.log("isStudent:", isStudent);
+*/
+
+/*
++ บวก
+- ลบ
+* คูณ
+/ หาร
+% หารเอาเศษ
+*/
+/*
+let number1 = 10;
+let number2 = 3;
+let result = number1 % number2;
+console.log("result:", result);
+*/
+
+/*
+== เท่ากับ 
+!= ไม่เท่ากับ
+> มากกว่า
+< น้อยกว่า
+>= มากกว่าหรือเท่ากับ
+<= น้อยกว่าหรือเท่ากับ
+*/
+/*
+let number1 = 10;
+let number2 = 20;
+
+let condition1 = number1 == number2; // false
+console.log("result of condition1 is:", condition1);
+*/
+
+/*
+let number1 = 25;
+let number2 = 25;
+//if else condition 
+if (number1 != number2) {
+    console.log ('this if')
+} else {
+    console.log ('this else')
+}
+*/
+
+/*
+let number1 = 25;
+let number2 = 25;
+//if - else condition 
+if (number1 != number2) {
+    console.log ('this if')
+} else if (number1 == number2) {
+    console.log ('this else if')
+} else {
+    console.log ('this else')
+}
+*/
+
+/*
+Grade
+>=80 เป็นเกรด A
+>=70 เป็นเกรด B
+>=60 เป็นเกรด C
+>=50 เป็นเกรด D
+<50 เป็นเกรด F
+*/
+
+/*let score = prompt("Enter your score:"); // ตัวอย่างคะแนน
+console.log("Your score is:", score);
+
+if (score >= 80) { //false
+   console.log('Your grade is:', 'A'); 
+} else if (score >= 70) { //true
+    console.log('Your grade is:', 'B'); //false
+} else if (score >= 60) {
+    console.log('Your grade is:', 'C'); //false
+} else if (score >= 50) {
+    console.log('Your grade is:', 'D'); //false
+} else {
+    console.log('Your grade is:', 'F'); //true
+}
+*/
+
+/*
+&& และ
+|| หรือ
+! ไม่ใช่
+
+
+let number1 = 5;
+let number2 = 8;
+//true && false
+let condition = ! (number1 >= 3 || number2 >= 10);
+console.log("condition:", condition);
+
+let age = 30;
+let gender = "ชาย";
+
+//  true && true = true
+if (age >= 20 && gender == "ชาย") {
+    console.log('ผู้ชายที่มีอายุ 20 ปีขึ้นไป');
+}
+
+*/
+
+/*
+let number = 20;
+
+if (number %2 != 0 ) {
+    console.log (number, 'เป็นเลขคู่')
+}
+*/
+
+/*let counter = 0;
+while (counter < 10) {
+    console.log('Hello World', counter);
+    counter = counter + 1;
+}
+
+for (let i = 0; i < 10; i ++) {
+    console.log('Hello World for loop', i);
+}
+*/
+
