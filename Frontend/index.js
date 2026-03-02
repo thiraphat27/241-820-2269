b2354951341b2ca4f5c954f9b6aa6da54ab55afe
thiraphat27 <thiraphat.chat@ku.th>
2026-03-02 17:56:12 +0700

    Add files via upload

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,13 +1,13 @@
-function submitData() {
-    let firstNameDom = document.querySelector('input[name=firstName]');
-    let lastNameDom = document.querySelector('input[name=lastName]');
+const submitData = async () => {
+    let firstNameDom = document.querySelector('input[name=firstname]')
+    let lastNameDom = document.querySelector('input[name=lastname]');
     let ageDom = document.querySelector('input[name=age]');
-
     let genderDom = document.querySelector('input[name=gender]:checked');
-    let interestDoms = document.querySelectorAll('input[name=interest]:checked');
-
+    let interestDoms = document.querySelectorAll('input[name=interests]:checked');
     let descriptionDom = document.querySelector('textarea[name=description]');
 
+    let messageDOM = document.getElementById('message');
+    try {
     let interest = ' '
     for (let i = 0; i < interestDoms.length; i++) {
         interest += interestDoms[i].value
@@ -20,9 +20,22 @@ function submitData() {
         firstName: firstNameDom.value,
         lastName: lastNameDom.value,
         age: ageDom.value,
-        gerder: genderDom.value,
-        descriptionDom: descriptionDom.value,
-        interest: interest
+        gender: genderDom.value,
+        description: descriptionDom.value,
+        interests: interest
     }
-    console.log('submit Data', userData);
+   
+        const response = await axios.post('http://localhost:8000/users', userData)
+        console.log('response', response.data);
+
+        messageDOM.innerText = "บันทึกข้อมูลสำเร็จ";
+        messageDOM.className = "message success";
+    } catch (error) {
+        if (error.response) {
+            console.error('Error response from server:', error.response.message);
+        }
+        messageDOM.innerText = "บันทึกข้อมูลไม่สำเร็จ";
+        messageDOM.className = "message danger";
+    }
+    
 }
